f4304031037c7bacd7c128de65df02e579d35cd5
George Jeng <gjenga@icloud.com>
2022-03-19 16:30:17 -0700

    Accessing username and password field values but no post req yet.
    Co-authored-by: Michelle Chang michelle.kelly.chang@gmail.com
    Co-authored-by: Jake Bradbeer jakebradbeer@gmail.com

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -7,12 +7,21 @@ const LoginForm = () => {
       <h1>Habitual</h1>
       <div className='wrapper-login'>
         <input type='text' className='login-input' id='username-input' name='username-input' placeholder='Enter Username' />
-        <input type='text' className='login-input' id='password-input' name='password-input' placeholder='Enter Password' />
-        <button className='login-btn' id='login-btn'>Login</button>
+        <input type='password' className='login-input' id='password-input' name='password-input' placeholder='Enter Password' />
+        <button className='login-btn' id='login-btn'
+          onClick={submitForm}
+        >Login</button>
         <div className='login-link' id='signup-btn'>Don't have an account? <br></br>Signup</div>
+        
       </div>
     </div>
   );
 };
+
+
+function submitForm () {
+  const username = document.querySelector('#username-input').value;
+  const password = document.querySelector('#password-input').value;
+}
 
 export default LoginForm;
